6e211d7f28cd40749997018dee2b5365ad865e09
Stacla3 <141387173+Stacla3@users.noreply.github.com>
2024-12-26 00:08:24 +0700

    add some js code

--- js/contact.js
+++ js/contact.js
@@ -1,22 +1,65 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+const receiveData = async function () {
+  try {
+    const urlData = await fetch("https://restcountries.com/v3.1/all");
+    if (!urlData.ok) throw new Error("Cannot get response.");
+    const data = await urlData.json();
+    const randomIndex = Math.floor(Math.random() * 100);
+    return data[randomIndex];
+  } catch (err) {
+    alert(err.message);
+  }
+};
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+const getPositionSuccess = function (p) {
+  const lat = p.coords.latitude;
+  const longi = p.coords.longitude;
+  creatMap(lat, longi);
+};
 
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup("London is beautiful city.")
-  .openPopup();
+const getPositionFail = function (err) {
+  switch (err.code) {
+    case err.PERMISSION_DENIED:
+      alert("User denied the request for Geolocation.");
+    default:
+      alert("Geolocaiton are not allow.");
+  }
+};
 
-var popup = L.popup();
+const checkGelocation = function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      getPositionSuccess,
+      getPositionFail
+    );
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+};
 
-function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(map);
-}
+const creatMap = function (lat, longi) {
+  var map = L.map("map").setView([lat, longi], 13);
 
-map.on("click", onMapClick);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([lat, longi])
+    .addTo(map)
+    .bindPopup("London is beautiful city.")
+    .openPopup();
+
+  var popup = L.popup();
+
+  function onMapClick(e) {
+    console.log(e);
+    popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(map);
+  }
+
+  map.on("click", onMapClick);
+};
+
+checkGelocation();
